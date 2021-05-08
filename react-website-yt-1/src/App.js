9d7f24eb633a81aa07f6ee6e91b04256fc808dc6
maleeshasilva <maleeshadinolisilva@gmail.com>
2021-05-08 20:07:26 +0530

    Update App.js

--- react-website-yt-1/src/App.js
+++ react-website-yt-1/src/App.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import Navbar from './components/Navbar';
-import './components/Navbar';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Home from './components/pages/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <Router>    
     <Navbar/>
     <Switch>
-      <route path='/' exact />
+    <Route path='/' exact component={Home} />
     </Switch>
     </Router>  
   );
